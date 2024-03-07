f3e094298dcb5b102011f5432b50d5ca6022dcb2
Kassa <nhokkho18@gmail.com>
2024-03-07 19:10:35 +0700

    done ex 6

--- Ex6_AddSubMulDiv_Var/app/src/main/java/com/example/ex6_addsubmuldiv_var/MainActivity.java
+++ Ex6_AddSubMulDiv_Var/app/src/main/java/com/example/ex6_addsubmuldiv_var/MainActivity.java
@@ -22,36 +22,68 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         timDieuKhien();
-        btnCong.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Code xử lý cộng
-                xuLyCong();
-            }
-        });
-
-        btnTru.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                xuLyTru();
-            }
-        });
-
-        btnNhan.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                xuLyNhan();
-            }
-        });
-
-        btnChia.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                xuLyChia();
-            }
-        });
+        btnCong.setOnClickListener(boLangNghe_xuLyCong);
+        btnTru.setOnClickListener(boLangNghe_xuLyTru);
+        btnNhan.setOnClickListener(boLangNghe_xuLyNhan);
+        btnChia.setOnClickListener(boLangNghe_xuLyChia);
     }
+    View.OnClickListener boLangNghe_xuLyCong = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            try {
+                double a = Double.parseDouble(editTextSoA.getText().toString());
+                double b = Double.parseDouble(editTextSoB.getText().toString());
 
+                editTextkq.setText(String.valueOf(a + b));
+            } catch (NumberFormatException e) {
+                Toast.makeText(MainActivity.this, "Vui lòng nhập số hợp lệ", Toast.LENGTH_SHORT).show();
+            }
+        }
+    };
+
+    View.OnClickListener boLangNghe_xuLyTru = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            try {
+                double a = Double.parseDouble(editTextSoA.getText().toString());
+                double b = Double.parseDouble(editTextSoB.getText().toString());
+
+                editTextkq.setText(String.valueOf(a-b));
+            } catch (NumberFormatException e) {
+                Toast.makeText(MainActivity.this, "Vui lòng nhập số hợp lệ", Toast.LENGTH_SHORT).show();
+            }
+        }
+    };
+
+    View.OnClickListener boLangNghe_xuLyNhan = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            try {
+                double a = Double.parseDouble(editTextSoA.getText().toString());
+                double b = Double.parseDouble(editTextSoB.getText().toString());
+
+                editTextkq.setText(String.valueOf(a*b));
+            } catch (NumberFormatException e) {
+                Toast.makeText(MainActivity.this, "Vui lòng nhập số hợp lệ", Toast.LENGTH_SHORT).show();
+            }
+        }
+    };
+
+    View.OnClickListener boLangNghe_xuLyChia = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            try {
+                double a = Double.parseDouble(editTextSoA.getText().toString());
+                double b = Double.parseDouble(editTextSoB.getText().toString());
+                if (b != 0)
+                    editTextkq.setText(String.valueOf(a/b));
+                else
+                    editTextkq.setText("Vui lòng nhập số thứ hai khác 0");
+            } catch (NumberFormatException e) {
+                Toast.makeText(MainActivity.this, "Vui lòng nhập số hợp lệ", Toast.LENGTH_SHORT).show();
+            }
+        }
+    };
     void timDieuKhien(){
         editTextSoA = (EditText)findViewById(R.id.edtA);
         editTextSoB = (EditText)findViewById(R.id.edtB);
@@ -61,50 +93,5 @@ public class MainActivity extends AppCompatActivity {
         btnNhan = (Button)findViewById(R.id.btnNhan);
         btnChia = (Button)findViewById(R.id.btnChia);
     }
-    public void xuLyCong()
-    {
-        try {
-            double a = Double.parseDouble(editTextSoA.getText().toString());
-            double b = Double.parseDouble(editTextSoB.getText().toString());
 
-            editTextkq.setText(String.valueOf(a+b));
-        } catch (NumberFormatException e) {
-            Toast.makeText(this, "Vui lòng nhập số hợp lệ", Toast.LENGTH_SHORT).show();
-        }
-    }
-    public void xuLyTru()
-    {
-        try {
-            double a = Double.parseDouble(editTextSoA.getText().toString());
-            double b = Double.parseDouble(editTextSoB.getText().toString());
-
-            editTextkq.setText(String.valueOf(a-b));
-        } catch (NumberFormatException e) {
-            Toast.makeText(this, "Vui lòng nhập số hợp lệ", Toast.LENGTH_SHORT).show();
-        }
-    }
-    public void xuLyNhan()
-    {
-        try {
-            double a = Double.parseDouble(editTextSoA.getText().toString());
-            double b = Double.parseDouble(editTextSoB.getText().toString());
-
-            editTextkq.setText(String.valueOf(a*b));
-        } catch (NumberFormatException e) {
-            Toast.makeText(this, "Vui lòng nhập số hợp lệ", Toast.LENGTH_SHORT).show();
-        }
-    }
-    public void xuLyChia()
-    {
-        try {
-            double a = Double.parseDouble(editTextSoA.getText().toString());
-            double b = Double.parseDouble(editTextSoB.getText().toString());
-            if (b != 0)
-                editTextkq.setText(String.valueOf(a/b));
-            else
-                editTextkq.setText("Vui lòng nhập số thứ hai khác 0");
-        } catch (NumberFormatException e) {
-            Toast.makeText(this, "Vui lòng nhập số hợp lệ", Toast.LENGTH_SHORT).show();
-        }
-    }
 }
