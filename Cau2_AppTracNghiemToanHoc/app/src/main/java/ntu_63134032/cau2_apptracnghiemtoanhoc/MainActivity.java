7de43ea0b859edf1abb9e0109cd71673f9cfbb6d
Kassa <nhokkho18@gmail.com>
2024-03-23 19:50:41 +0700

    bắt đầu làm backend

--- Cau2_AppTracNghiemToanHoc/app/src/main/java/ntu_63134032/cau2_apptracnghiemtoanhoc/MainActivity.java
+++ Cau2_AppTracNghiemToanHoc/app/src/main/java/ntu_63134032/cau2_apptracnghiemtoanhoc/MainActivity.java
@@ -1,6 +1,9 @@
 package ntu_63134032.cau2_apptracnghiemtoanhoc;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,11 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btnA;
+    private Button btnB;
+    private Button btnC;
+    private Button btnD;
+    private TextView tvDe;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +28,15 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        getControls();
+    }
+
+    public void getControls(){
+        btnA = (Button) findViewById(R.id.bnt_a);
+        btnB = (Button) findViewById(R.id.bnt_b);
+        btnC = (Button) findViewById(R.id.bnt_c);
+        btnD = (Button) findViewById(R.id.bnt_d);
+        tvDe = (TextView) findViewById(R.id.tv_de);
     }
 }
