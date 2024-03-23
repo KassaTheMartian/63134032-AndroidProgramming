6b4fc3659c294076cf1484e72b6e27cb6c47e3ff
Kassa <nhokkho18@gmail.com>
2024-03-23 22:05:06 +0700

    merse ans, merse quiz

--- Cau2_AppTracNghiemToanHoc/app/src/main/java/ntu_63134032/cau2_apptracnghiemtoanhoc/MainActivity.java
+++ Cau2_AppTracNghiemToanHoc/app/src/main/java/ntu_63134032/cau2_apptracnghiemtoanhoc/MainActivity.java
@@ -12,7 +12,9 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
@@ -24,6 +26,8 @@ public class MainActivity extends AppCompatActivity {
     private Button btnC;
     private Button btnD;
     private TextView tvDe;
+    private TextView tvScore;
+    private TextView tvTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,8 +48,7 @@ public class MainActivity extends AppCompatActivity {
         listQuiz.add(new Quiz(3));
         listQuiz.add(new Quiz(3));
         listQuiz.add(new Quiz(3));
-
-        tvDe.setText(listQuiz.get(count).getChuoiPhepToan());
+        newGame();
         btnA.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -73,28 +76,65 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void chonDapAn(Button btn){
-        Random random = new Random();
         if (btn.getText().toString().equals(String.valueOf(listQuiz.get(count).tinhChuoiPhepToan()))){
             countKq++;
             count++;
-            btnA.setText(String.valueOf(listQuiz.get(count).tinhChuoiPhepToan()));
-            btnB.setText(String.valueOf(listQuiz.get(count).tinhChuoiPhepToan()));
-            btnC.setText(String.valueOf(listQuiz.get(count).tinhChuoiPhepToan()));
-            btnD.setText(String.valueOf(listQuiz.get(count).tinhChuoiPhepToan()));
+            setAns(listQuiz.get(count));
         }else {
             count++;
-            btnA.setText(String.valueOf(listQuiz.get(count).tinhChuoiPhepToan()));
-            btnB.setText(String.valueOf(listQuiz.get(count).tinhChuoiPhepToan()));
-            btnC.setText(String.valueOf(listQuiz.get(count).tinhChuoiPhepToan()));
-            btnD.setText(String.valueOf(listQuiz.get(count).tinhChuoiPhepToan()));
+            setAns(listQuiz.get(count));
         }
     }
+
+    public void mergeQuiz() {
+        Collections.shuffle(listQuiz);
+    }
+    public void newGame(){
+        mergeQuiz();
+        count = 0;
+        countKq = 0;
+        tvScore.setText("Score\n" + (countKq));
+        tvDe.setText(listQuiz.get(count).getChuoiPhepToan());
+        setAns(listQuiz.get(count));
+    }
+    public void setAns(Quiz quiz){
+        Random random = new Random();
+        ArrayList<Double> options = new ArrayList<>();
+
+        options.add(roundToTwoDecimalPlaces(quiz.tinhChuoiPhepToan()));
+        options.add(roundToTwoDecimalPlaces(random.nextDouble() * 15));
+        options.add(roundToTwoDecimalPlaces(random.nextDouble() * 15));
+        options.add(roundToTwoDecimalPlaces(random.nextDouble() * 15));
+
+        Collections.shuffle(options); // Trộn thứ tự các lựa chọn
+
+        btnA.setText(String.valueOf(options.get(0)));
+        btnB.setText(String.valueOf(options.get(1)));
+        btnC.setText(String.valueOf(options.get(2)));
+        btnD.setText(String.valueOf(options.get(3)));
+
+        tvDe.setText(listQuiz.get(count).getChuoiPhepToan());
+
+        tvScore.setText("Score\n" + (countKq));
+
+    }
+
+    public static double roundToTwoDecimalPlaces(double value) {
+        DecimalFormat df = new DecimalFormat("#.#");
+        return Double.parseDouble(df.format(value));
+    }
+
+
+
     public void getControls(){
         btnA = (Button) findViewById(R.id.bnt_a);
         btnB = (Button) findViewById(R.id.bnt_b);
         btnC = (Button) findViewById(R.id.bnt_c);
         btnD = (Button) findViewById(R.id.bnt_d);
         tvDe = (TextView) findViewById(R.id.tv_de);
+        tvScore = (TextView) findViewById(R.id.tv_score);
+        tvTime = (TextView) findViewById(R.id.tv_time);
+
     }
 
 
