556887b3deb6110b099a7e8f7274c016be3516ae
Kassa <nhokkho18@gmail.com>
2024-03-23 21:36:22 +0700

    nghien cuu

--- Cau2_AppTracNghiemToanHoc/app/src/main/java/ntu_63134032/cau2_apptracnghiemtoanhoc/MainActivity.java
+++ Cau2_AppTracNghiemToanHoc/app/src/main/java/ntu_63134032/cau2_apptracnghiemtoanhoc/MainActivity.java
@@ -2,6 +2,7 @@ package ntu_63134032.cau2_apptracnghiemtoanhoc;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -11,7 +12,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
+    private ArrayList<Quiz> listQuiz = new ArrayList<Quiz>();
+    private int count = 0;
+    private int countKq = 0;
     private Button btnA;
     private Button btnB;
     private Button btnC;
@@ -30,8 +37,58 @@ public class MainActivity extends AppCompatActivity {
         });
 
         getControls();
+
+        listQuiz.add(new Quiz(3));
+        listQuiz.add(new Quiz(3));
+        listQuiz.add(new Quiz(3));
+        listQuiz.add(new Quiz(3));
+        listQuiz.add(new Quiz(3));
+        listQuiz.add(new Quiz(3));
+
+        tvDe.setText(listQuiz.get(count).getChuoiPhepToan());
+        btnA.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                chonDapAn(btnA);
+            }
+        });
+        btnB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                chonDapAn(btnB);
+            }
+        });
+        btnC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                chonDapAn(btnC);
+            }
+        });
+        btnD.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                chonDapAn(btnD);
+            }
+        });
     }
 
+    public void chonDapAn(Button btn){
+        Random random = new Random();
+        if (btn.getText().toString().equals(String.valueOf(listQuiz.get(count).tinhChuoiPhepToan()))){
+            countKq++;
+            count++;
+            btnA.setText(String.valueOf(listQuiz.get(count).tinhChuoiPhepToan()));
+            btnB.setText(String.valueOf(listQuiz.get(count).tinhChuoiPhepToan()));
+            btnC.setText(String.valueOf(listQuiz.get(count).tinhChuoiPhepToan()));
+            btnD.setText(String.valueOf(listQuiz.get(count).tinhChuoiPhepToan()));
+        }else {
+            count++;
+            btnA.setText(String.valueOf(listQuiz.get(count).tinhChuoiPhepToan()));
+            btnB.setText(String.valueOf(listQuiz.get(count).tinhChuoiPhepToan()));
+            btnC.setText(String.valueOf(listQuiz.get(count).tinhChuoiPhepToan()));
+            btnD.setText(String.valueOf(listQuiz.get(count).tinhChuoiPhepToan()));
+        }
+    }
     public void getControls(){
         btnA = (Button) findViewById(R.id.bnt_a);
         btnB = (Button) findViewById(R.id.bnt_b);
@@ -39,4 +96,6 @@ public class MainActivity extends AppCompatActivity {
         btnD = (Button) findViewById(R.id.bnt_d);
         tvDe = (TextView) findViewById(R.id.tv_de);
     }
+
+
 }
--- Cau2_AppTracNghiemToanHoc/app/src/main/java/ntu_63134032/cau2_apptracnghiemtoanhoc/Quiz.java
+++ Cau2_AppTracNghiemToanHoc/app/src/main/java/ntu_63134032/cau2_apptracnghiemtoanhoc/Quiz.java
@@ -11,8 +11,8 @@ public class Quiz {
         this.chuoiPhepToan = phepToan;
     }
 
-    public Quiz(){
-
+    public Quiz(int n){
+        this.chuoiPhepToan = taoChuoiPhepToan(n);
     }
 
     public String taoChuoiPhepToan(int n){
@@ -32,5 +32,67 @@ public class Quiz {
         }
 
         return builder.toString();
+    }
+    public double tinhChuoiPhepToan() {
+        Stack<Double> numbers = new Stack<>();
+        Stack<Character> toanTu = new Stack<>();
+
+        for (int i = 0; i < chuoiPhepToan.length(); i++) {
+            char c = chuoiPhepToan.charAt(i);
+            if (Character.isDigit(c)) {
+                StringBuilder sb = new StringBuilder();
+                sb.append(c);
+                while (i + 1 < chuoiPhepToan.length() && Character.isDigit(chuoiPhepToan.charAt(i + 1))) {
+                    sb.append(chuoiPhepToan.charAt(++i));
+                }
+                numbers.push(Double.parseDouble(sb.toString()));
+            } else if (c == '+' || c == '-' || c == '*' || c == '/') {
+                while (!toanTu.empty() && uuTien(toanTu.peek()) >= uuTien(c)) {
+                    apDungToanTu(numbers, toanTu);
+                }
+                toanTu.push(c);
+            }
+        }
+
+        while (!toanTu.empty()) {
+            apDungToanTu(numbers, toanTu);
+        }
+
+        return numbers.pop();
+    }
+
+    public int uuTien(char operator) {
+        if (operator == '+' || operator == '-') {
+            return 1;
+        } else if (operator == '*' || operator == '/') {
+            return 2;
+        }
+        return -1;
+    }
+
+    public void apDungToanTu(Stack<Double> numbers, Stack<Character> operators) {
+        double b = numbers.pop();
+        double a = numbers.pop();
+        char operator = operators.pop();
+        double result = 0.0;
+        switch (operator) {
+            case '+':
+                result = a + b;
+                break;
+            case '-':
+                result = a - b;
+                break;
+            case '*':
+                result = a * b;
+                break;
+            case '/':
+                result = a / b;
+                break;
+        }
+        numbers.push(result);
+    }
+
+    public String getChuoiPhepToan() {
+        return chuoiPhepToan;
     }
 }
